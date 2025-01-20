499a66b96eebc20b91d64853754d8ea839fb3914
CRISTE0 <escuderocristian65@gmail.com>
2025-01-20 01:13:36 -0500

    test1

--- src/main/java/com/Challenge/ForoHub/ForoHubApplication.java
+++ src/main/java/com/Challenge/ForoHub/ForoHubApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ForoHubApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ForoHubApplication.class, args);
+//		SpringApplication.run(ForoHubApplication.class, args);
+		System.out.println("Siuuuu MR.INCREIBLE");
 	}
 
 }
